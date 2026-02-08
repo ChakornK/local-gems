972afd6fc28cdee05bb8d7f12fd29c49cf019d08
ChakornK <chakornk2007@gmail.com>
2026-02-08 02:05:00 -0800

    feat: add bottom sheet component

---
+++ src/components/BottomSheet.jsx
@@ -0,0 +1,47 @@
+import { AnimatePresence, motion } from "motion/react";
+import { createPortal } from "react-dom";
+
+export default function BottomSheet({ open, onClose, children }) {
+  const overlayVariants = {
+    visible: { opacity: 1 },
+    hidden: { opacity: 0 },
+  };
+
+  const sheetVariants = {
+    visible: {
+      y: 0,
+      transition: { ease: "easeInOut", duration: 0.15 },
+    },
+    hidden: {
+      y: "100%",
+      transition: { ease: "anticipate" },
+    },
+  };
+
+  return createPortal(
+    <AnimatePresence>
+      {open && (
+        <>
+          <motion.div
+            className="z-67670 fixed inset-0 bg-black/50"
+            onClick={onClose}
+            variants={overlayVariants}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+          />
+          <motion.div
+            className="border-b-128 z-67670 fixed bottom-0 left-1/2 top-8 -mb-32 w-full max-w-xl -translate-x-1/2 rounded-t-xl border-gray-800 bg-gray-800 text-white shadow-lg"
+            variants={sheetVariants}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+          >
+            {children}
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>,
+    document.body,
+  );
+}
